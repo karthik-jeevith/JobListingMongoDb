fffcc4f2a7d943643888c1a3c37e931f99e07152
karthik-jeevith <ckarthikmuthu1234@gmail.com>
2024-04-10 09:22:59 +0530

    making deafult APIs ignored in swagger page

--- src/main/java/com/careerportal/joblisting/Repository/PostRepo.java
+++ src/main/java/com/careerportal/joblisting/Repository/PostRepo.java
@@ -4,6 +4,8 @@ import com.careerportal.joblisting.models.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+
+//while connecting mondo-db cloud use mobile hotspot and make sure IP is added in custer.
 @Repository
 public interface PostRepo extends MongoRepository<Post,String> {
 
--- src/main/java/com/careerportal/joblisting/controller/PostController.java
+++ src/main/java/com/careerportal/joblisting/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,6 +18,7 @@ public class PostController {
     @Autowired
     PostRepo repo;
 
+    @ApiIgnore
     @RequestMapping(value = "/")
     public void redirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("swagger-ui.html");
