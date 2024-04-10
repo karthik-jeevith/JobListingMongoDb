ff75cda6537682d3b225234dde799688b026ae31
karthik-jeevith <ckarthikmuthu1234@gmail.com>
2024-04-10 09:15:17 +0530

    changed the swagger dependency version to 2.9.2

--- src/main/java/com/careerportal/joblisting/controller/PostController.java
+++ src/main/java/com/careerportal/joblisting/controller/PostController.java
@@ -1,17 +1,31 @@
 package com.careerportal.joblisting.controller;
 
 
+import com.careerportal.joblisting.Repository.PostRepo;
+import com.careerportal.joblisting.models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 @RestController
 public class PostController {
 
+    @Autowired
+    PostRepo repo;
+
     @RequestMapping(value = "/")
     public void redirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("swagger-ui.html");
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts()
+    {
+        return repo.findAll();
     }
 
 }
